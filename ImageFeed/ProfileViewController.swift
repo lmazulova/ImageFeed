bceb0f118f5bac12a83939184c046656f67e181b
Elizabeth <lmazulova@bk.ru>
2024-12-10 16:04:30 +0300

    реализация показа изображения через navigation bar

---
+++ ImageFeed/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by user on 06.12.2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet weak var profilePhoto: UIImageView!
+    @IBOutlet weak var exitBtn: UIButton!
+    // MARK: - IB Outlets
+    // MARK: - Properties
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ ImageFeed/SingleImageViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by user on 10.12.2024.
+//
+
+import Foundation
