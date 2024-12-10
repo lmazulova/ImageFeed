f70dcfc569c47e2e3734021db1b939a5be5926b7
Elizabeth <lmazulova@bk.ru>
2024-12-10 16:05:56 +0300

    реализация navigation bar

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -19,11 +19,27 @@ final class ImagesListViewController: UIViewController {
         formatter.timeStyle = .none
         return formatter
     }()
+    private let ShowSingleImageSegueIdentifier = "ShowSingleImage"
     
     // MARK: - Overrides Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         TableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == ShowSingleImageSegueIdentifier {
+            guard let viewController = segue.destination as? SingleImageViewController,
+                  let indexPath = sender as? IndexPath
+            else {
+                assertionFailure("Invalid segue destination")
+                return
+            }
+            let image = UIImage(named: photosName[indexPath.row])
+            viewController.image = image
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
     }
 }
 
@@ -51,6 +67,7 @@ extension ImagesListViewController: UITableViewDataSource {
 extension ImagesListViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: ShowSingleImageSegueIdentifier, sender: indexPath)
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -81,6 +98,6 @@ extension ImagesListViewController {
         guard let image = UIImage(named: photosName[indexPath.row]) else {return}
         cell.ImageView.image = image
         cell.dataLabel.text = dateFormatter.string(from: Date())
-        cell.likeButton.imageView?.image = UIImage(named: indexPath.row % 2 == 0 ? "icActive" : "noActive")
+        cell.likeButton.imageView?.image = UIImage(named: indexPath.row % 2 == 0 ? "activeLike" : "inactiveLike")
     }
 }
--- ImageFeed/SingleImageViewController.swift
+++ ImageFeed/SingleImageViewController.swift
@@ -5,4 +5,13 @@
 //  Created by user on 10.12.2024.
 //
 
-import Foundation
+import UIKit
+final class SingleImageViewController: UIViewController {
+    var image: UIImage?
+    @IBOutlet weak private var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
+}
