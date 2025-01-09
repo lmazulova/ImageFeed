cac84e8805499a4fb28caad95ff93323bef7d9a9
Elizabeth <lmazulova@bk.ru>
2025-01-09 15:30:07 +0300

    feat: добавила updateProfileDetails(profile: Profile), ошибка с подгрузкой ProgressHUD

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -42,42 +42,32 @@ final class ProfileViewController: UIViewController {
     // MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        guard let token = OAuth2TokenStorage().token else {return}
-        ProfileService().fetchProfile(token){
-            result in
-            switch result {
-            case .success(let Profile):
-                let name = self.addLabel(text: Profile.name)
-                name.font = UIFont.systemFont(ofSize: 23, weight: .bold)
-                let tag = self.addLabel(text: Profile.loginName)
-                tag.textColor = .ypGrey
-                let description = self.addLabel(text: Profile.bio)
-                let profile = self.addImageView(imageName: "profilePhoto")
-                let exitButton = self.addButton(imageName: "Exit")
-                
-                NSLayoutConstraint.activate([
-                    profile.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 32),
-                    profile.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-                    profile.heightAnchor.constraint(equalToConstant: 70),
-                    profile.widthAnchor.constraint(equalToConstant: 70),
-                    name.topAnchor.constraint(equalTo: profile.bottomAnchor, constant: 8),
-                    tag.topAnchor.constraint(equalTo: name.bottomAnchor, constant: 8),
-                    description.topAnchor.constraint(equalTo: tag.bottomAnchor, constant: 8),
-                    exitButton.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -24),
-                    exitButton.centerYAnchor.constraint(equalTo: profile.centerYAnchor),
-                    exitButton.heightAnchor.constraint(equalToConstant: 22),
-                    exitButton.widthAnchor.constraint(equalToConstant: 20)
-                ])
-            case .failure(let error):
-                print(error)
-                break
-        }
-        }
+        guard let profile = ProfileService.shared.profile else {return}
+        self.updateProfileDetails(profile: profile)
     }
     
     private func updateProfileDetails(profile: Profile) {
+        let name = self.addLabel(text: profile.name)
+        name.font = UIFont.systemFont(ofSize: 23, weight: .bold)
+        let tag = self.addLabel(text: profile.loginName)
+        tag.textColor = .ypGrey
+        let description = self.addLabel(text: profile.bio)
+        let profile = self.addImageView(imageName: "profilePhoto")
+        let exitButton = self.addButton(imageName: "Exit")
         
+        NSLayoutConstraint.activate([
+            profile.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            profile.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            profile.heightAnchor.constraint(equalToConstant: 70),
+            profile.widthAnchor.constraint(equalToConstant: 70),
+            name.topAnchor.constraint(equalTo: profile.bottomAnchor, constant: 8),
+            tag.topAnchor.constraint(equalTo: name.bottomAnchor, constant: 8),
+            description.topAnchor.constraint(equalTo: tag.bottomAnchor, constant: 8),
+            exitButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -24),
+            exitButton.centerYAnchor.constraint(equalTo: profile.centerYAnchor),
+            exitButton.heightAnchor.constraint(equalToConstant: 22),
+            exitButton.widthAnchor.constraint(equalToConstant: 20)
+        ])
     }
     
     @objc
@@ -88,5 +78,4 @@ final class ProfileViewController: UIViewController {
             }
         }
     }
-    
 }
